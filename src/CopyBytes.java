988442e93b32f9a6c9c88f5e6e5dce0a1ef360c9
Danh <luongdanh125@gmail.com>
2022-11-29 16:56:06 +0700

    Initial commit

---
+++ src/CopyBytes.java
@@ -0,0 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class CopyBytes {
+    public static void copy(String source, String destination) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+    }
+}
