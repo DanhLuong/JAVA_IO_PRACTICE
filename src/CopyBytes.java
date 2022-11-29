0e6b407cfcecccf963727a9cc0942cb7a74291b4
Danh <luongdanh125@gmail.com>
2022-11-29 18:12:42 +0700

    improve

--- src/CopyBytes.java
+++ src/CopyBytes.java
@@ -1,10 +1,32 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.sql.SQLOutput;
 
 public class CopyBytes {
-    public static void copy(String source, String destination) {
-        FileInputStream in = null;
-        FileOutputStream out = null;
+    public static void copy(String sourcePath, String destinationPath) throws IOException {
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+        int temp;
+        try {
+            File outFile = new File(destinationPath);
+            if(!outFile.exists()) {
+                inStream = new FileInputStream(sourcePath);
+                outStream = new FileOutputStream(destinationPath);
+                while ((temp=inStream.read())!=-1) {
+                    outStream.write(temp);
+                }
+            }else {
+                System.out.println("Error: Duplicate name. Can not copy.");
+            }
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (inStream!=null) {
+                inStream.close();
+            }
+            if(outStream!=null){
+                outStream.close();
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        CopyBytes.copy("C:\\Users\\Danh\\IdeaProjects\\JAVA_IO_PRACTICE\\out\\production\\JAVA_IO_PRACTICE\\sourceTestFilePDF.pdf", "C:\\Users\\Danh\\IdeaProjects\\JAVA_IO_PRACTICE\\out\\production\\JAVA_IO_PRACTICE\\newDuplicate.pdf");
+
     }
 }
